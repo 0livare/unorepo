7f51de1ee4e6e21712564e00f6f9c7f9df481f26
Zach Posten <zach@posten.io>
2019-07-28 00:34:32 -0500

    refactor: Use consistent language in watch command

--- src/commands/watch.js
+++ src/commands/watch.js
@@ -5,21 +5,19 @@ const logger = require('../util/logger')
 const buildDependencyChain = require('../util/buildDependencyChain')
 
 /*
- * Watch for changes in each of the packages in this workspace
+ * Watch for changes in each of the packages in this project
  */
 async function watch(cmd) {
   try {
     const packageInfo = await getPackagesInfo()
     createWatcher(packageInfo, cmd.script)
   } catch (error) {
-    throw new Error(
-      `There was a problem trying to watch the workspace: ${error}`,
-    )
+    throw new Error(`There was a problem watching the project: ${error}`)
   }
 }
 
 /**
- * Get the package information for all (public) packages in the current workspace
+ * Get the package information for all (public) packages in the project
  * @returns {
  *   name: The name of the package,
  *   version: The version of the package,
@@ -28,16 +26,9 @@ async function watch(cmd) {
  * }
  */
 async function getPackagesInfo() {
-  try {
-    const lernaArgs = ['ls', '--toposort', '--json']
-    const {stdout, stderr} = await execa('lerna', lernaArgs)
-
-    return JSON.parse(stdout)
-  } catch (error) {
-    throw new Error(
-      `There was an error getting the workspace packages: ${error}`,
-    )
-  }
+  const lernaArgs = ['ls', '--toposort', '--json']
+  const {stdout, stderr} = await execa('lerna', lernaArgs)
+  return JSON.parse(stdout)
 }
 
 async function createWatcher(packagesInfo, scriptToRunOnChange) {
@@ -50,10 +41,9 @@ async function createWatcher(packagesInfo, scriptToRunOnChange) {
   // prettier-ignore
   let watcher = chokidar.watch(packagesPaths, {
     ignored: [
-      /(^|[\/\\])\../,    // Ignore dot files
-      /node_modules/,     // Ignore node_modules
-      /lib|dist/,         // Ignore build output files
-      /\*___jb_tmp___/,   // Ignore jetbrains IDE temp files
+      /lib|dist|build|bld/, // Ignore build output
+      /node_modules/,       // Ignore node_modules
+      /(^|[\/\\])\..+$/,    // Ignore dot files
     ],
     persistent: true,
     ignoreInitial: true,
@@ -63,15 +53,15 @@ async function createWatcher(packagesInfo, scriptToRunOnChange) {
   // Add event listeners
   return watcher
     .on('add', path => {
-      logger.blue(`File ${path} has been added`)
+      logger.blue(`File ${path} was added`)
       buildDependencyChain(path, scriptToRunOnChange)
     })
     .on('change', path => {
-      logger.blue(`File ${path} has been changed`)
+      logger.blue(`File ${path} was changed`)
       buildDependencyChain(path, scriptToRunOnChange)
     })
     .on('unlink', path => {
-      logger.blue(`File ${path} has been removed`)
+      logger.blue(`File ${path} was removed`)
       buildDependencyChain(path, scriptToRunOnChange)
     })
 }
